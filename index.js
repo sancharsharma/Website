e191d45962e9e89688da46e577f5915ab80d3712
Sanchar Sharma <sancharsharma@gmail.com>
2026-05-12 18:16:30 +0200

    Code cleanup

--- index.js
+++ index.js
@@ -1,66 +1,39 @@
-var app = document.getElementById('app');
-
-var typewriter = new Typewriter(app, {
-  loop: false,
-  delay: 25,
-});
-
-typewriter
-  .pauseFor(1000)
-  .typeString('I am a physicist interested in quantum magnonics') //Add your own tagline
-  .pauseFor(3000)
-  .start()
-
-
-var granimInstance = new Granim({
-    element: '#canvas-image-blending',
-    direction: 'top-bottom',
-    isPausedWhenNotInView: true,
-    image : {
-        source: 'MainImageResized.jpg', //change image for intro section if desired
-        blendingMode: 'multiply',
-    },
-    states : {
-        "default-state": {
-            gradients: [
-                ['#29323c', '#485563'],
-                ['#FF6B6B', '#556270'],
-                ['#80d3fe', '#7ea0c4'],
-                ['#f0ab51', '#eceba3']
-            ],
-            transitionSpeed: 8000
-        }
-    }
-});
+(function () {
+    var text = 'I am a physicist interested in quantum magnonics';
+    var el = document.getElementById('typewriter-text');
+    var i = 0;
+    setTimeout(function () {
+        var timer = setInterval(function () {
+            el.textContent += text[i];
+            i++;
+            if (i === text.length) clearInterval(timer);
+        }, 50);
+    }, 800);
+}());
 
 function PubShowHide(list_pubs, button_pubs) {
-	var publist = document.getElementById(list_pubs);
-	var pubbutton = document.getElementById(button_pubs);
+    var publist = document.getElementById(list_pubs);
+    var pubbutton = document.getElementById(button_pubs);
 
-	if (publist.style.display == "") {
-		publist.style.display = "block";
-		pubbutton.innerHTML = "Click to hide the publication list";
-	} else {
-		publist.style.display = "";
-		pubbutton.innerHTML = "Click to show relevant publications";
-	}
+    if (publist.style.display == "") {
+        publist.style.display = "block";
+        pubbutton.innerHTML = "Click to hide the publication list";
+    } else {
+        publist.style.display = "";
+        pubbutton.innerHTML = "Click to show relevant publications";
+    }
 }
 
 function ToScroll(loc) {
-	var Id = document.getElementById(loc);
-	Id.scrollIntoView();
+    document.getElementById(loc).scrollIntoView();
 }
-
 
 function SummaryShowHide(summary, pap_title) {
-	var summ = document.getElementById(summary);
-	var title = document.getElementById(pap_title);
+    var summ = document.getElementById(summary);
 
-	if (summ.style.display == "") {
-		summ.style.display = "block";
-	} else {
-		summ.style.display = "";
-	}
+    if (summ.style.display == "") {
+        summ.style.display = "block";
+    } else {
+        summ.style.display = "";
+    }
 }
-
-
